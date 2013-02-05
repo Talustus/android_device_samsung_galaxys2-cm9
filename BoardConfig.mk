7f8ff0265cd6758d4d83001f08031402e76924d6
Talustus <talustus@dream-irc.de>
2013-02-05 22:01:51 +0100

    Add TWRP settings
    Change-Id: I7805c8068c2b8c63a025f1481729315adfa9dad2

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -63,6 +63,138 @@ BOARD_SYSTEMIMAGE_PARTITION_SIZE := 536870912
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 2147483648
 BOARD_FLASH_BLOCK_SIZE := 4096
 
+#########################
+#### TWRP Recovery ####
+#### Common Options ####
+#########################
+# Targetv RECOVERY RC
+# TARGET_RECOVERY_INITRC := device/samsung/galaxys2/recovery/init.rc
+
+# Screen Resulution the device uses
+DEVICE_RESOLUTION := 480x800
+
+# fixes slanty looking graphics on some devices
+# RECOVERY_GRAPHICS_USE_LINELENGTH := true
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+
+# Custom Source for recovery reboot functions
+# TARGET_RECOVERY_REBOOT_SRC := custom-reboot.c
+
+# enable/disable the reboot recovery button on the reboot menu, if u want to disable it for some reason ....
+TW_NO_REBOOT_RECOVERY := false
+
+# enable/disable the reboot bootloader button on the reboot menu, if your device uses Bootloader Mode instead of Download Mode
+TW_NO_REBOOT_BOOTLOADER := true
+
+# enable/disable the reboot downloadmode button on the reboot menu, if your device uses Download Mode instead of Bootloader Mode
+TW_HAS_DOWNLOAD_MODE := true
+
+# many Samsung Devices do not use a seperate Recovery partition, if thats your case set it to true! ...
+TW_HAS_NO_RECOVERY_PARTITION := true
+
+# this enables proper handling of /data/media on devices that have this folder for storage
+# RECOVERY_SDCARD_ON_DATA := true
+
+# enable/disable things like sdcard partitioning etc
+BOARD_HAS_NO_REAL_SDCARD := false
+
+# Path to the internal Storage
+TW_INTERNAL_STORAGE_PATH := "/emmc"
+
+# Mountpoint for the internal Storage
+TW_INTERNAL_STORAGE_MOUNT_POINT := "emmc"
+
+# Path to the external Storage
+TW_EXTERNAL_STORAGE_PATH := "/sdcard"
+
+# Mountpoint for the external Storage
+TW_EXTERNAL_STORAGE_MOUNT_POINT := "sdcard"
+
+# FUSE_EXFAT support (TWRP 2.4)
+TW_INCLUDE_FUSE_EXFAT := true
+
+# External Storage as default for backups?
+TW_DEFAULT_EXTERNAL_STORAGE := false
+
+# Always keep system mounted
+TW_NEVER_UNMOUNT_SYSTEM := false
+
+# Enable flashing of ZIP's form their current location instead of copying them to /tmp
+TW_FLASH_FROM_STORAGE := true
+
+# Always use rm -rf instead of format
+TW_ALWAYS_RMRF := false
+
+# enables/disables USB Storage Support
+TW_NO_USB_STORAGE := false
+
+# External SD does not use EXT4 Filesystem (VFAT ...)
+TW_SDEXT_NO_EXT4 := true
+
+# Custom Power KEY
+# TW_CUSTOM_POWER_BUTTON := 103
+
+# uncomment for HTC Devices only
+# TW_INCLUDE_DUMLOCK := true
+
+# adds ability to inject TWRP into some Samsung boot images for Samsung devices that have recovery as a second ramdisk in the boot image
+TW_INCLUDE_INJECTTWRP := false
+
+# uses blobpack to restore boot images (Asus Transformer devices)
+TW_INCLUDE_BLOBPACK := false
+
+# Uses CPU infos for a unique Device identifier
+TW_FORCE_CPUINFO_FOR_DEVICE_ID := false
+
+# enable/disable the display of the battery percentage for devices that don't support it properly
+TW_NO_BATT_PERCENT := false
+
+# Enable ICS Crypto Support
+TW_INCLUDE_CRYPTO := false
+
+# Enable JellyBean Crypto Support, this also enables ICS crypto support !
+TW_INCLUDE_JB_CRYPTO := true
+
+# support for Samsung crypto (TWRP 2.4)
+# TW_INCLUDE_CRYPTO_SAMSUNG := true
+
+# Crypto related settings
+# TW_CRYPTO_FS_TYPE := "ext4"
+# TW_CRYPTO_REAL_BLKDEV := "/dev/block/mmcblk0p10"
+# TW_CRYPTO_MNT_POINT := "/data"
+# TW_CRYPTO_FS_OPTIONS := "noauto_da_alloc"
+# TW_CRYPTO_FS_FLAGS := "0x00000006"
+# TW_CRYPTO_KEY_LOC := "/efs/metadata"
+
+# enables touch event logging to help debug touchscreen issues
+TWRP_EVENT_LOGGING := false
+
+######################################
+## Additional Partitions to backup ##
+## define each in recovery.fstab ##
+######################################
+## PART1 - EFS (mmcblk0p21)
+# SP1_NAME := "efs"
+# SP1_DISPLAY_NAME := "efs"
+# SP1_BACKUP_METHOD := files
+# SP1_MOUNTABLE := 1
+
+## PART2 - TOMBSTONES (mmcblk0p27)
+# SP2_NAME := "tombstones"
+# SP2_DISPLAY_NAME := "tombstones"
+# SP2_BACKUP_METHOD := files
+# SP2_MOUNTABLE := 1
+
+## PART3
+# SP3_NAME := "PARAM"
+# SP3_DISPLAY_NAME := "PARAM LFS Backup Image"
+# SP3_BACKUP_METHOD := image
+# SP3_MOUNTABLE := 0
+
+#########################
+### END TWRP SETTINGS ###
+#########################
+
 # Releasetools
 TARGET_RELEASETOOL_OTA_FROM_TARGET_SCRIPT := ./device/samsung/galaxys2/releasetools/galaxys2_ota_from_target_files
 TARGET_RELEASETOOL_IMG_FROM_TARGET_SCRIPT := ./device/samsung/galaxys2/releasetools/galaxys2_img_from_target_files
